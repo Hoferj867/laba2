9935c9c756f6df7821d5b84eaccce0d45b818d3b
hoferj <55890926+Hoferj867@users.noreply.github.com>
2019-11-07 19:51:45 +0200

    Add files via upload

---
+++ LAb_2_1/LAb_2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LAb_2_1
+{
+    namespace ConsoleApplication6
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите a,b,c");
+                double a = Convert.ToInt32(Console.ReadLine());
+                double b = Convert.ToInt32(Console.ReadLine());
+                double c = Convert.ToInt32(Console.ReadLine());
+                double dis = (b * b - 4 * a * c);
+                if (dis > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(dis)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(dis)) / (2 * a);
+                    Console.WriteLine("x1=" + x1 + "x2=" + x2);
+
+                }
+                else if (dis == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine("x=" + x);
+                }
+                else
+                {
+                    double y = 0;5
+
+                    Console.WriteLine("x + i * y");
+                    Console.WriteLine("x - i * y");
+                }
+            }
+        }
+    }
+}
---
+++ laba_2_2/laba_2_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace laba_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество сгаемых: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a <= 0)
+            {
+                Console.WriteLine("ошибка");
+            }
+            else
+            {
+                double Pi = 1;
+                for (double i = 1, del = 3; i <= a; i++, del += 2)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Pi -= 1 / del;
+                    }
+                    if (i % 2 == 0)
+                    {
+                        Pi += 1 / del;
+                    }
+                }
+                Pi *= 4;
+                Console.WriteLine("Pi = " + Pi);
+                Console.ReadKey();
+            }
+        }
+    }
+}
