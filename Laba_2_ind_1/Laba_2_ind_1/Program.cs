c5969406f51b5be3d753485b58cadcec8f14b633
hoferj <55890926+Hoferj867@users.noreply.github.com>
2019-11-14 21:23:44 +0300

    Add files via upload

---
+++ Laba_2_ind_1/Laba_2_ind_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Laba_2_ind_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите день:");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц:");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (day > 9 && day < 32)
+            {
+                Console.Write(day + ".");
+            }
+            else
+            {
+                Console.Write("0" + day+".");
+            }
+            if(month>9 && month<13)
+                {
+                Console.Write(month + ".");
+                }
+            else
+            {
+                Console.Write("0" + month + ".");
+            }
+             if (year > 0 && year < 5000)
+            {
+                Console.Write(year);
+            }
+            
+        }
+    }
+}
---
+++ Laba_2_ind_2/Laba_2_ind_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Laba_2_ind_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а:");
+            int a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            int b= Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (a < b)
+            {
+                for(int i= a;i<b;i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += i;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+           
+        }
+    }
+}
